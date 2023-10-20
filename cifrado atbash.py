7301a4eeef995457bb23baec352f32a4cc9711f1
Leandro Contestabile <83560286+leancontestabile@users.noreply.github.com>
2023-10-20 13:15:38 -0300

    implementación de firma y doctest.py

--- cifrado atbash.py
+++ cifrado atbash.py
@@ -1,4 +1,42 @@
 def cifrado_atbash(cadena):
+    """
+    Recibe una cadena por parametro y devuelve una cadena cifrada o descifrada con el metodo atbash
+    
+    El objetivo es cifrar o descifrar el mensaje que recibe la funcion, sustituyendo sus caracteres alfanumericos por su inverso
+    
+    Contestabile Leandro Ezequiel
+    
+    >>> cifrado_atbash("hola mundo")
+    'SLOZ ÑFNWL'
+    
+    >>> cifrado_atbash("!hola#mundo$")
+    '!SLOZ#ÑFNWL$'
+
+    >>> cifrado_atbash("HOLA MUNDO")
+    'sloz ñfnwl'
+    
+    >>> cifrado_atbash("0123 9876")
+    '9876 0123'
+    
+    >>> cifrado_atbash("PEDROCARRASCO.ORG")
+    'kvwilxziizhxl.lit'
+    
+    >>> cifrado_atbash("kvwilxziizhxl.lit")
+    'PEDROCARRASCO.ORG'
+    
+    >>> cifrado_atbash("Somos el equipo Argentina")
+    'hLÑLH VO VJFRKL zITVNGRNZ'
+    
+    >>> cifrado_atbash("Messi es el 10!")
+    'ñVHHR VH VO 89!'
+    
+    >>> cifrado_atbash("hLÑLH XZÑKVLNVH WVO ÑFNWL")
+    'Somos campeones del mundo'
+    
+    >>> cifrado_atbash("Pru3b4")
+    'kIF6Y5'
+    """
+    
     lista_caracteres = []
     numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     numeros_invertidos = ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0"]
@@ -11,17 +49,13 @@ def cifrado_atbash(cadena):
             lista_caracteres.append(letras_minuscula[letras_invertidas_mayuscula.index(letra)])
         elif letra.isdigit():
             lista_caracteres.append(numeros[numeros_invertidos.index(letra)])
-        elif letra not in letras_minuscula:
+        else:
             lista_caracteres.append(letra)
     nueva_cadena = "".join(lista_caracteres)
-    print(nueva_cadena)
+    return nueva_cadena
 
 def main():
-    cifrado_atbash("hola mundo")
-    cifrado_atbash("!hola#mundo$")
-    cifrado_atbash("HOLA MUNDO")
-    cifrado_atbash("0123 9876")
-    cifrado_atbash("PEDROCARRASCO.ORG")
-    cifrado_atbash("kvwilxziizhxl.lit")
+    import doctest
+    print(doctest.testmod())
 
 main()

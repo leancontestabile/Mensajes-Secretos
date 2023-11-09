0ab02ec7084da3d73bab75960aed4b838cc3e551
Leandro Contestabile <83560286+leancontestabile@users.noreply.github.com>
2023-11-09 14:19:18 -0300

    verificación usuario emisor y remitente

--- consultar_mensajes_cifrados.py
+++ consultar_mensajes_cifrados.py
@@ -1,15 +1,21 @@
 def consultar_mensajes(id_usuario):
     total_mensajes = 0
     print("Lista de mensajes:")
-    
-    #leer linea del archivo
     with open("prueba_mensajes.csv", "r") as ar_mensajes:
         for linea in ar_mensajes:
             emisor, receptor, cifrado, mensaje_cifrado = linea.rstrip("\n").split(",")
-            print(emisor, receptor, cifrado, mensaje_cifrado)
-    
-    #verificar que el mensaje sea para el (si el mensaje es un all anteponer un # al id del remitente)
-    #mostrarlo descifrado
+            if (receptor == id_usuario):
+                if (cifrado == "A"):
+                    mensaje_descifrado = cifrado_atbash(mensaje_cifrado)
+                    print(emisor,":", mensaje_descifrado)
+                else:
+                    print(emisor,":", mensaje_cifrado)
+                total_mensajes += 1
+            elif (receptor == "*") and (emisor != id_usuario):
+                if (cifrado == "A"):
+                    mensaje_descifrado = cifrado_atbash(mensaje_cifrado)
+                    print("#",emisor,":", mensaje_descifrado)
+                else:
+                    print("#",emisor,":", mensaje_cifrado)
+                total_mensajes += 1
     print("Total de mensajes:", total_mensajes)
-    
-consultar_mensajes("lean")

505121c61c67f0d3dff2e4e7ef0a8b163ec7989c
Leandro Contestabile <83560286+leancontestabile@users.noreply.github.com>
2023-11-16 15:23:40 -0300

    agregado orden mensajes recibidos

--- consultar_mensajes_cifrados.py
+++ consultar_mensajes_cifrados.py
@@ -1,4 +1,7 @@
 def consultar_mensajes(id_usuario):
+#     implementacion funciones y agregar docstring
+    mensajes_generales = []
+    mensajes_personales = []
     total_mensajes = 0
     print("Lista de mensajes:")
     with open("prueba_mensajes.csv", "r") as ar_mensajes:
@@ -7,17 +10,25 @@ def consultar_mensajes(id_usuario):
             if (receptor == id_usuario):
                 if (cifrado == "A"):
                     mensaje_descifrado = cifrado_atbash(mensaje_cifrado)
-                    print(emisor,":", mensaje_descifrado)
+                    aux_mensaje = emisor +": " + mensaje_descifrado
+                    mensajes_personales.append(aux_mensaje)
                 else:
                     mensaje_descifrado = cifrado_cesar(mensaje_cifrado, -int(cifrado[1]))
-                    print(emisor,":", mensaje_descifrado)
+                    aux_mensaje = emisor +": " + mensaje_descifrado
+                    mensajes_personales.append(aux_mensaje)
                 total_mensajes += 1
             elif (receptor == "*") and (emisor != id_usuario):
                 if (cifrado == "A"):
                     mensaje_descifrado = cifrado_atbash(mensaje_cifrado)
-                    print("#",emisor,":", mensaje_descifrado)
+                    aux_mensaje = "#" + emisor +": " + mensaje_descifrado
+                    mensajes_generales.append(aux_mensaje)
                 else:
                     mensaje_descifrado = cifrado_cesar(mensaje_cifrado, -int(cifrado[1]))
-                    print("#",emisor,":", mensaje_descifrado)
+                    aux_mensaje = "#" + emisor +": " + mensaje_descifrado
+                    mensajes_generales.append(aux_mensaje)
                 total_mensajes += 1
+    for i in mensajes_generales:
+        print(i)
+    for i in mensajes_personales:
+        print(i)
     print("Total de mensajes:", total_mensajes)

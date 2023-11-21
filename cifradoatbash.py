5aad76b5c6eaccc2c35c099ef7e3784f9a1bd9b2
Leandro Contestabile <83560286+leancontestabile@users.noreply.github.com>
2023-11-21 11:08:12 -0300

    eliminacion letra ñ.py

--- cifradoatbash.py
+++ cifradoatbash.py
@@ -40,8 +40,8 @@ def cifrado_atbash(cadena):
     lista_caracteres = []
     numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     numeros_invertidos = ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0"]
-    letras_minuscula = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    letras_invertidas_mayuscula = ["Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "Ñ", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"]
+    letras_minuscula = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    letras_invertidas_mayuscula = ["Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"]
     for letra in cadena:
         if letra.islower():
             lista_caracteres.append(letras_invertidas_mayuscula[letras_minuscula.index(letra)])

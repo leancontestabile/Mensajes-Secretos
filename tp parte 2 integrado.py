b07e7b2fb45f0bdbb6646e497f77e1528a72108f
0OoBRIANoO0 <129856327+0OoBRIANoO0@users.noreply.github.com>
2023-11-28 13:21:48 -0300

    Update tp parte 2 integrado.py

--- tp parte 2 integrado.py
+++ tp parte 2 integrado.py
@@ -136,13 +136,16 @@ def crear_usuario(usuario,clave,id_pregunta_seguridad, respuesta):
                     messagebox.showinfo("completado","Identificador guardado")
                 elif not usuario_insertado and usuario_existente:
                     messagebox.showwarning("error","Identificador en uso")
+                    
+        archivo_usuarios.close()
+        nuevo_archivo_usuarios.close()
+        remove("usuario_clave.csv")
+        rename("nuevo_usuario_clave.csv","usuario_clave.csv")
+    
     elif not usuario_valido or not clave_valida:
         messagebox.showwarning("eror","clave o usuario invalidos")
 
-    archivo_usuarios.close()
-    nuevo_archivo_usuarios.close()
-    remove("usuario_clave.csv")
-    rename("nuevo_usuario_clave.csv","usuario_clave.csv")
+    
 
     return usuario_insertado
 

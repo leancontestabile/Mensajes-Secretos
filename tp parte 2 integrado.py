739b47f05f2c6d7b3a59700d270a6b369e3ac265
Leandro Contestabile <83560286+leancontestabile@users.noreply.github.com>
2023-11-29 15:00:37 -0300

    correccion cifrado atbash repetido

--- tp parte 2 integrado.py
+++ tp parte 2 integrado.py
@@ -416,49 +416,22 @@ def boton_ingresar_registro(raiz,input_usuario,input_contraseña,combo_var,input
 
 # objetivo 2: cifrado atbash
 def cifrado_atbash(cadena):
+    
     """
+
     Recibe una cadena por parametro y devuelve una cadena cifrada o descifrada con el metodo atbash
     
     El objetivo es cifrar o descifrar el mensaje que recibe la funcion, sustituyendo sus caracteres alfanumericos por su inverso
     
     Contestabile Leandro Ezequiel
-    
-    >>> cifrado_atbash("hola mundo")
-    'SLOZ ÑFNWL'
-    
-    >>> cifrado_atbash("!hola#mundo$")
-    '!SLOZ#ÑFNWL$'
 
-    >>> cifrado_atbash("HOLA MUNDO")
-    'sloz ñfnwl'
-    
-    >>> cifrado_atbash("0123 9876")
-    '9876 0123'
-    
-    >>> cifrado_atbash("PEDROCARRASCO.ORG")
-    'kvwilxziizhxl.lit'
-    
-    >>> cifrado_atbash("kvwilxziizhxl.lit")
-    'PEDROCARRASCO.ORG'
-    
-    >>> cifrado_atbash("Somos el equipo Argentina")
-    'hLÑLH VO VJFRKL zITVNGRNZ'
-    
-    >>> cifrado_atbash("Messi es el 10!")
-    'ñVHHR VH VO 89!'
-    
-    >>> cifrado_atbash("hLÑLH XZÑKVLNVH WVO ÑFNWL")
-    'Somos campeones del mundo'
-    
-    >>> cifrado_atbash("Pru3b4")
-    'kIF6Y5'
     """
     
     lista_caracteres = []
     numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     numeros_invertidos = ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0"]
-    letras_minuscula = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    letras_invertidas_mayuscula = ["Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "Ñ", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"]
+    letras_minuscula = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    letras_invertidas_mayuscula = ["Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"]
     for letra in cadena:
         if letra.islower():
             lista_caracteres.append(letras_invertidas_mayuscula[letras_minuscula.index(letra)])
@@ -469,7 +442,6 @@ def cifrado_atbash(cadena):
         else:
             lista_caracteres.append(letra)
     nueva_cadena = "".join(lista_caracteres)
-    
     return nueva_cadena
 
 mensaje_cifrado_global = "" #Variable global
@@ -1079,8 +1051,6 @@ def boton_recuperar_contraseña(raiz,input_usuario,combo_var,input_respuesta):
         else:
             interfaz_inicial()
             
-
-
 def boton_ingresar_registro(raiz,input_usuario,input_contraseña,combo_var,input_respuesta):
 
     usuario = input_usuario.get()
@@ -1093,66 +1063,6 @@ def boton_ingresar_registro(raiz,input_usuario,input_contraseña,combo_var,input
     if usuario_insertado:
         raiz.destroy()
         interfaz_login()
-
-
-
-# objetivo 2: cifrado atbash
-def cifrado_atbash(cadena):
-    """
-    Recibe una cadena por parametro y devuelve una cadena cifrada o descifrada con el metodo atbash
-    
-    El objetivo es cifrar o descifrar el mensaje que recibe la funcion, sustituyendo sus caracteres alfanumericos por su inverso
-    
-    Contestabile Leandro Ezequiel
-    
-    >>> cifrado_atbash("hola mundo")
-    'SLOZ ÑFNWL'
-    
-    >>> cifrado_atbash("!hola#mundo$")
-    '!SLOZ#ÑFNWL$'
-
-    >>> cifrado_atbash("HOLA MUNDO")
-    'sloz ñfnwl'
-    
-    >>> cifrado_atbash("0123 9876")
-    '9876 0123'
-    
-    >>> cifrado_atbash("PEDROCARRASCO.ORG")
-    'kvwilxziizhxl.lit'
-    
-    >>> cifrado_atbash("kvwilxziizhxl.lit")
-    'PEDROCARRASCO.ORG'
-    
-    >>> cifrado_atbash("Somos el equipo Argentina")
-    'hLÑLH VO VJFRKL zITVNGRNZ'
-    
-    >>> cifrado_atbash("Messi es el 10!")
-    'ñVHHR VH VO 89!'
-    
-    >>> cifrado_atbash("hLÑLH XZÑKVLNVH WVO ÑFNWL")
-    'Somos campeones del mundo'
-    
-    >>> cifrado_atbash("Pru3b4")
-    'kIF6Y5'
-    """
-    
-    lista_caracteres = []
-    numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    numeros_invertidos = ["9", "8", "7", "6", "5", "4", "3", "2", "1", "0"]
-    letras_minuscula = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    letras_invertidas_mayuscula = ["Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "Ñ", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"]
-    for letra in cadena:
-        if letra.islower():
-            lista_caracteres.append(letras_invertidas_mayuscula[letras_minuscula.index(letra)])
-        elif letra.isupper():
-            lista_caracteres.append(letras_minuscula[letras_invertidas_mayuscula.index(letra)])
-        elif letra.isdigit():
-            lista_caracteres.append(numeros[numeros_invertidos.index(letra)])
-        else:
-            lista_caracteres.append(letra)
-    nueva_cadena = "".join(lista_caracteres)
-    
-    return nueva_cadena
 
 # objetivo 3: interfaz grafica de usuario
 
@@ -1501,3 +1411,4 @@ def mostrar_resultado(texto_cuadro, titulo, texto):
     texto_cuadro.config(state=DISABLED)
 
 interfaz_inicial()
+
